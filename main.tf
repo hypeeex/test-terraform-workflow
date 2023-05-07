8fa3fbdff1be2f8183083b14be072f569095848f
virtuoso <hypeeex@gmail.com>
2023-05-08 01:50:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
 
+resource "local_file" "pyfile" {
+  filename = "hello.py"
+  content = <<-EOT
+    def hello():
+        print("Hello, World!")
+    hello()
+  EOT
+}
+
+output "output_file" {
+  value = local_file.pyfile.filename
+}
