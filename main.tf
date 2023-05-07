3ce80e730349c6f1ffa84e470304273b5226be02
virtuoso <hypeeex@gmail.com>
2023-05-08 01:53:12 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,19 @@
-
-resource "local_file" "pyfile" {
-  filename = "hello.py"
-  content = <<-EOT
-    def hello():
-        print("Hello, World!")
-    hello()
-  EOT
+# Zdefiniuj zmienne
+variable "file_path" {
+  default = "welcome.txt"
 }
 
-output "output_file" {
-  value = local_file.pyfile.filename
+variable "content" {
+  default = "${file("${path.module}/welcome.py")}"
+}
+
+# Utwórz zasób
+resource "local_file" "welcome_file" {
+  filename = var.file_path
+  content  = var.content
+}
+
+# Wywołaj funkcję Pythona
+provisioner "local-exec" {
+  command = "python3 ${path.module}/welcome.py"
 }
